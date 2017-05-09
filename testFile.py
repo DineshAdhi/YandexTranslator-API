5662a511c8601bc3f3097e68c3952d530a76effc
Dinesh <dinsesh10c04@gmail.com>
2017-05-09 07:56:00 +0530

    Initial Commit

--- testFile.py
+++ testFile.py
@@ -1,6 +1,6 @@
 import YandexTranslator
 
-key = "trnsl.1.1.20170508T175121Z.c2202e77aa3bb4a7.7d73016a5863e323330bc11d1ba6a32f73dda0a2"
+key = <API-KEY>
 
 translator = YandexTranslator.translator(key);
 
